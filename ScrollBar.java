e7d8c1002d86c34f7659dd08dd53b1a21112c399
ashutosh9938 <ashutosh.201752@ncit.edu.np>
2023-01-06 09:28:11 +0545

    initial

---
+++ ScrollBar.java
@@ -0,0 +1,27 @@
+//scroll bar
+import javax.swing.*;
+import java.awt.event.*;
+public class ScrollBar {
+
+    ScrollBar(){
+        JFrame f=new JFrame();
+        f.setSize(400,200);
+        JLabel l=new JLabel();
+        l.setBounds(100,400,500,200);
+        l.setVerticalAlignment(JLabel.RIGHT);
+        l.setSize(400,500);
+        JScrollBar s=new JScrollBar();
+        s.setBounds(100,400,500,200);
+        f.add(l);f.add(s);
+        f.setVisible(true);
+        f.setLayout(null);
+        s.addAdjustmentListener(new AdjustmentListener(){
+            public void adjustmentValueChanged(AdjustmentEvent e){
+                l.setText("nth"+s.getValue());
+            }
+        });
+    }
+    public static void main(String[]args){
+        new ScrollBar();
+    }
+}
